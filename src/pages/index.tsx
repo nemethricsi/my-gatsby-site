df8a978bdcf2db3b0cb36214c6b9b43adf73e5a4
Richard Nemeth <nemethricsi@gmail.com>
2024-10-22 19:48:32 +0200

    add an image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Link, type HeadFC, type PageProps } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
@@ -7,8 +8,13 @@ const IndexPage: React.FC<PageProps> = () => {
       <h1 className="text-4xl font-bold">Hello World</h1>
       <div className="flex gap-4">
         <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
       </div>
+      <StaticImage
+        src="../images/honey3.png"
+        alt="Honey 3"
+        quality={100}
+        placeholder="blurred"
+      />
     </main>
   );
 };
